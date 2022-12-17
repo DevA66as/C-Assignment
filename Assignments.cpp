c7601ce4a699730ab89fe9f56e31d0b0f8c2eff8
Abbas Mohammed <99032870+DevA66as@users.noreply.github.com>
2022-12-17 22:46:01 +0300

    Add files via upload

---
+++ Assignments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  cout << "Enter A Number To Find Its Square";
+  //*Declare A Variable
+  int squareRoot;
+  //*Enter The Variable Value
+  cin>>squareRoot;
+  //*Finding The Square Root To The Entered Number
+  cout<<"The Square Root To The Entered Number\n"<<sqrt(squareRoot);
+  return 0;
+}
